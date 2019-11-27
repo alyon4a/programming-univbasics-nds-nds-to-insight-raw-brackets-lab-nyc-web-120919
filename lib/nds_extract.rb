015ef8758303bca0ed2759a93375e2cb3063949c
Olena Matokhina <olena@matokhina.com>
2019-11-27 15:40:35 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -17,7 +17,7 @@ def directors_totals(nds)
       total_grosses += movies_array[movies_iterator][:worldwide_gross]
     end
     
-    hash_result[director[:name]] => total_grosses
+    hash_result[director[:name]] = total_grosses
   end
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
